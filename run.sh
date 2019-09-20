7a9f6725594284f7c730d67aad0e886365604f04
Jesús Maillo Hidalgo <jesusmh@decsai.ugr.es>
2019-09-20 16:41:49 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 spark-submit --master local[*] --class org.apache.spark.run.runMetrics ./target/ComplexityMetrics-1.0.jar <path-to-dataset> <path-to-output> ClassicMetrics 10
-spark-submit --master local[*] --class org.apache.spark.run.runMetrics ./target/Metrics-1.0.jar <path-to-dataset> <path-to-output> ND 10
-spark-submit --master local[*] --class org.apache.spark.run.runMetrics ./target/Metrics-1.0.jar <path-to-dataset> <path-to-output> DTP 10
+spark-submit --master local[*] --class org.apache.spark.run.runMetrics ./target/ComplexityMetrics-1.0.jar <path-to-dataset> <path-to-output> ND 10
+spark-submit --master local[*] --class org.apache.spark.run.runMetrics ./target/ComplexityMetrics-1.0.jar <path-to-dataset> <path-to-output> DTP 10
 
